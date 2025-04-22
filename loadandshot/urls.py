404f378daebc7efe7d600443d20bfecffea7cbca
LoadAndShot <loadandshot@gmail.com>
2025-04-22 19:31:48 +0200

    Update urls.py

--- loadandshot/urls.py
+++ loadandshot/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from orders import views  # 🟢 Import de la vue accueil
 
 urlpatterns = [
+    path('', views.home_view, name='home'),  # 🟢 Page d'accueil
     path('admin/', admin.site.urls),
-    path('', include('orders.urls')),
+    path('', include('orders.urls')),       # 🟢 Les autres routes (login, register, etc.)
 ]
