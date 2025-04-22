d48ad1659f07afe47b5596ce684cd1051d95ab4b
LoadAndShot <loadandshot@gmail.com>
2025-04-22 18:57:39 +0200

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # On ajoutera les vues ici après
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
