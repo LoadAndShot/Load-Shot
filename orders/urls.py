79ecf69fa841b26e9c54339e7e6cbd4ffd2a1202
LoadAndShot <loadandshot@gmail.com>
2025-04-22 19:09:15 +0200

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('order/', views.create_order_view, name='create_order'),
 ]
