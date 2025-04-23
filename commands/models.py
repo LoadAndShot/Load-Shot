825f1bd28cbbb8c552db76b37006058ccbd2fff5
LoadAndShot <loadandshot@gmail.com>
2025-04-23 14:55:11 +0200

    Update models.py

--- commands/models.py
+++ commands/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.conf import settings  # Pour utiliser correctement le CustomUser
+from django.conf import settings
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    catalogue = models.IntegerField(choices=[(1, 'Catalogue 1'), (2, 'Catalogue 2')], default=1)
+    catalogue = models.IntegerField(choices=[(1, 'Catalogue 1'), (2, 'Catalogue 2')])
 
     def __str__(self):
         return self.name
@@ -14,12 +14,10 @@ class Order(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    delivery_method = models.CharField(max_length=50, choices=[('retrait', 'Retrait'), ('livraison', 'Livraison')])
     created_at = models.DateTimeField(auto_now_add=True)
-    delivery_method = models.CharField(
-        max_length=20,
-        choices=[('retrait', 'Retrait'), ('livraison', 'Livraison')],
-        default='retrait'
-    )
+    is_delivered = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Commande de {self.client.username} - {self.product.name}"
+
