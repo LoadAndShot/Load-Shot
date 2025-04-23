431a96f3a750eada60c5e203874fc427f8e719c8
LoadAndShot <loadandshot@gmail.com>
2025-04-23 08:07:27 +0200

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Product, Order
+from .models import Order
 
-admin.site.register(Product)
-admin.site.register(Order)
+# Configuration de l'affichage du modèle Order dans l'interface d'administration
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product_type', 'method', 'created_at', 'sent_to_discord')
+    list_filter = ('product_type', 'method', 'sent_to_discord')
+    search_fields = ('user__username',)
