32981ac5091630d795c4813ff59b13cfaf85e8d7
LoadAndShot <loadandshot@gmail.com>
2025-04-23 14:43:42 +0200

    Update admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -4,8 +4,12 @@ from .models import Product, Order
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'catalogue')
+    list_filter = ('catalogue',)
+    search_fields = ('name',)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('client', 'product', 'quantity', 'delivery_method', 'created_at')
-    list_filter = ('delivery_method',)
+    list_filter = ('delivery_method', 'created_at')
+    search_fields = ('client__username', 'product__name')
+
