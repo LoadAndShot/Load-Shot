fdb14be66d717ad5e2b62b770a14bee3c59570e0
LoadAndShot <loadandshot@gmail.com>
2025-04-24 00:19:04 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    can_access_catalogue1 = models.BooleanField(default=True)
-    can_access_catalogue2 = models.BooleanField(default=True)
+    can_access_catalogue1 = models.BooleanField(default=False)
+    can_access_catalogue2 = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)  # Numéro IG
+    password = models.CharField(max_length=128, null=True, blank=True)  # ✅ La ligne à ajouter/corriger
 
+    def __str__(self):
+        return self.username
