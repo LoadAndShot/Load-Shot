4e571f82006df2c39e43b4a7d480d78323db5903
LoadAndShot <loadandshot@gmail.com>
2025-04-23 13:43:09 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    is_admin = models.BooleanField(default=False)
+    # Si tu veux rajouter des champs supplémentaires
+    pass
