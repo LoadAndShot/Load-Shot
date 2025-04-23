d73fbd92397df24628b169235c0d0c2668548cd6
LoadAndShot <loadandshot@gmail.com>
2025-04-23 10:37:55 +0200

    Update models.py

--- commands/models.py
+++ commands/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.conf import settings
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    catalogue = models.PositiveSmallIntegerField(choices=[(1, 'Catalogue 1'), (2, 'Catalogue 2')])
+    catalogue = models.IntegerField(choices=[(1, 'Catalogue 1'), (2, 'Catalogue 2')])
 
     def __str__(self):
         return self.name
@@ -12,9 +13,10 @@ class Product(models.Model):
 class Order(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    delivery_method = models.CharField(max_length=100, choices=[('pickup', 'Retrait sur place'), ('delivery', 'Livraison')])
+    quantity = models.PositiveIntegerField()
+    delivery_method = models.CharField(max_length=50, choices=[('Retrait', 'Retrait'), ('Livraison', 'Livraison')])
     created_at = models.DateTimeField(auto_now_add=True)
+    is_locked = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Commande de {self.client.username} - {self.product.name}"
