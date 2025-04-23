34f485b21f1ef7eb50bd29678ee3f85c2245fffd
LoadAndShot <loadandshot@gmail.com>
2025-04-23 10:31:26 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    is_locked_catalogue1 = models.BooleanField(default=False)
-    is_locked_catalogue2 = models.BooleanField(default=False)
+    # Ici tu peux ajouter des champs si besoin, exemple :
+    is_verified = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Utilisateur'
+        verbose_name_plural = 'Utilisateurs'
