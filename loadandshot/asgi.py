a0df532923f308edf38559390d5504fddf5ef13b
LoadAndShot <loadandshot@gmail.com>
2025-04-23 08:32:14 +0200

    Update asgi.py

--- loadandshot/asgi.py
+++ loadandshot/asgi.py
@@ -5,5 +5,5 @@ Expose la variable ASGI `application` pour les déploiements asynchrones.
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loadshot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loadandshot.settings')
 application = get_asgi_application()
