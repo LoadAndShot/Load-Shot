1592ac4f045b4c2665d4ce8c4fe1613e07457eb3
LoadAndShot <loadandshot@gmail.com>
2025-04-23 08:58:02 +0200

    Delete orders directory

--- orders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Order
-
-# Configuration de l'affichage du modèle Order dans l'interface d'administration
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'product_type', 'method', 'created_at', 'sent_to_discord')
-    list_filter = ('product_type', 'method', 'sent_to_discord')
-    search_fields = ('user__username',)
