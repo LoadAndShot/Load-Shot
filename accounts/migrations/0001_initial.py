3378ba754a0f5fa0ca50c7033a06ab08493b632f
LoadAndShot <loadandshot@gmail.com>
2025-04-23 18:55:10 +0200

    Update 0001_initial.py

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -1,10 +1,20 @@
-migrations.CreateModel(
-    name='CustomUser',
-    fields=[
-        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-        ('username', models.CharField(max_length=150, unique=True)),
-        ('can_access_catalogue1', models.BooleanField(default=True)),
-        ('can_access_catalogue2', models.BooleanField(default=True)),
-        # ... les autres champs ...
-    ],
-)
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='CustomUser',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.CharField(max_length=150, unique=True)),
+                ('can_access_catalogue1', models.BooleanField(default=True)),
+                ('can_access_catalogue2', models.BooleanField(default=True)),
+                # ... autres champs ...
+            ],
+        ),
+    ]
