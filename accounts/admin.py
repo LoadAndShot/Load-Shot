ffac7fc3db16dcadee25d4e47f5b1e599ee86d7a
LoadAndShot <loadandshot@gmail.com>
2025-04-23 13:43:32 +0200

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from commands.models import Product, Order
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'catalogue')
-    list_filter = ('catalogue',)
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('client', 'product', 'quantity', 'delivery_method', 'created_at')
-    list_filter = ('delivery_method', 'created_at')
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
