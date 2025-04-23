c00f992e7b4439414e879f9e56cec4d67652ab3b
LoadAndShot <loadandshot@gmail.com>
2025-04-23 23:31:14 +0200

    Update views.py

--- commands/views.py
+++ commands/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Product, Order
+from bot.discord_notifications import send_discord_notification
+from django.conf import settings
 import requests
 import os
 
