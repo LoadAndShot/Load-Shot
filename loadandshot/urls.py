8b4f77eef3121f5e8a6032226a49b58619bf0ccf
LoadAndShot <loadandshot@gmail.com>
2025-04-23 11:02:48 +0200

    Update urls.py

--- loadandshot/urls.py
+++ loadandshot/urls.py
@@ -1,8 +1,14 @@
+# loadandshot/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('orders.urls')),
+    path('', include('accounts.urls')),  # Routing vers les pages login/register/home
+    path('commands/', include('commands.urls')),  # Routing vers les catalogues et commandes
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
