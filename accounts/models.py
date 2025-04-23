96b28f3c21703da8919448be52ca217011b7e910
LoadAndShot <loadandshot@gmail.com>
2025-04-23 16:38:59 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Si tu veux rajouter des champs personnalisés plus tard, c’est ici
-    pass
+    can_access_catalogue1 = models.BooleanField(default=True)
+    can_access_catalogue2 = models.BooleanField(default=True)
 
