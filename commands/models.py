25418218ad2c3c90ea8a6e05f9f334165f6594b5
LoadAndShot <loadandshot@gmail.com>
2025-04-24 00:47:42 +0200

    Update models.py

--- commands/models.py
+++ commands/models.py
@@ -13,14 +13,18 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
+    DELIVERY_METHOD_CHOICES = [
+        ('pickup', 'Retrait sur place'),
+        ('delivery', 'Livraison'),
+    ]
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     phone_number = models.CharField(max_length=100)
+    delivery_method = models.CharField(max_length=20, choices=DELIVERY_METHOD_CHOICES)
     is_delivered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Commande de {self.user.username} - {self.product.name}"
-
-
