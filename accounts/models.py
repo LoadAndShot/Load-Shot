9b2a62167489a372482875fef40acece64085559
LoadAndShot <loadandshot@gmail.com>
2025-04-24 00:24:06 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     can_access_catalogue1 = models.BooleanField(default=False)
     can_access_catalogue2 = models.BooleanField(default=False)
-    phone_number = models.CharField(max_length=255, blank=True, null=True)  # Numéro IG
-    password = models.CharField(max_length=128, null=True, blank=True)  # ✅ La ligne à ajouter/corriger
+    phone_number = models.CharField(max_length=50, blank=True, null=True)  # Numéro IG, libre
+    password = models.CharField(max_length=128, null=True, blank=True)  # OK pour éviter le bug migration
 
     def __str__(self):
         return self.username
+
