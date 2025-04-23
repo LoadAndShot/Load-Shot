24b3bfc2429dbdc3ea3b719fbe3db38de8ad8c86
LoadAndShot <loadandshot@gmail.com>
2025-04-24 00:36:48 +0200

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,22 +1,35 @@
+# accounts/admin.py
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('username', 'email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active', 'is_superuser')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+    # Définir les champs visibles dans les sections de l'admin
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('username', 'password')}),
+        (_('Informations personnelles'), {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Accès Catalogues'), {
+            'fields': ('can_access_catalogue1', 'can_access_catalogue2'),
+        }),
+        (_('Dates importantes'), {'fields': ('last_login', 'date_joined')}),
     )
+
+    # Champs pour l'ajout d'un nouvel utilisateur (affiche can_access_catalogue1/2 même à la création)
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}
+            'fields': ('username', 'email', 'password1', 'password2', 'phone_number', 'can_access_catalogue1', 'can_access_catalogue2', 'is_staff', 'is_active')}
         ),
     )
+
+    # Colonnes affichées dans la liste des utilisateurs
+    list_display = ('username', 'email', 'is_staff', 'can_access_catalogue1', 'can_access_catalogue2')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'can_access_catalogue1', 'can_access_catalogue2', 'groups')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
