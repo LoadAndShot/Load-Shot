84a3af5b3f60e8ddb7614f754dc880cdf40eec66
LoadAndShot <loadandshot@gmail.com>
2025-04-23 08:06:00 +0200

    Update wsgi.py

--- loadandshot/wsgi.py
+++ loadandshot/wsgi.py
@@ -1,7 +1,10 @@
+"""
+WSGI config for Load&Shot project.
+Expose la variable WSGI `application` pour le déploiement web.
+"""
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loadandshot.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loadshot.settings')
 application = get_wsgi_application()
 
