9d674cdd642d4fb751f8ee428f5a828b2f4f9d56
LoadAndShot <loadandshot@gmail.com>
2025-04-23 23:51:09 +0200

    Update models.py

--- commands/models.py
+++ commands/models.py
@@ -13,12 +13,14 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
-    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    delivery_method = models.CharField(max_length=20, choices=[('livraison', 'Livraison'), ('retrait', 'Retrait')])
-    phone_number = models.CharField(max_length=50)  # ✅ Ici tu ajoutes le numéro de téléphone IG
+    phone_number = models.CharField(max_length=100)  # Ton ajout précédent
+    is_delivered = models.BooleanField(default=False)  # ✅ ajoute cette ligne
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Commande de {self.product.name} par {self.client.username}"
+        return f"Commande de {self.user.username} - {self.product.name}"
+
