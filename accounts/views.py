5e193d217a0b5cb5ff85dc94abd7b1659b0c9fa9
LoadAndShot <loadandshot@gmail.com>
2025-04-23 15:05:45 +0200

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from commands.models import Product, Order
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
 
 @login_required
 def dashboard(request):
