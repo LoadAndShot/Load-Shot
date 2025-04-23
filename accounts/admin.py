463a91a31847a2aa3732c84e8f8403d8b8c7c701
LoadAndShot <loadandshot@gmail.com>
2025-04-23 10:01:02 +0200

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_locked_catalogue1', 'is_locked_catalogue2')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Catalogue Locks', {'fields': ('is_locked_catalogue1', 'is_locked_catalogue2')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
