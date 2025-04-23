b570721a863692f1398a9a4073cf5930df7fb9f5
LoadAndShot <loadandshot@gmail.com>
2025-04-23 10:37:40 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,5 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Ici tu peux ajouter des champs si besoin, exemple :
-    is_verified = models.BooleanField(default=False)
-
-    class Meta:
-        verbose_name = 'Utilisateur'
-        verbose_name_plural = 'Utilisateurs'
+    is_admin = models.BooleanField(default=False)
