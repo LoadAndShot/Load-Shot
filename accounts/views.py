0554a6ec5c491c34d1e0fd9a574aaaf533375515
LoadAndShot <loadandshot@gmail.com>
2025-04-23 13:51:54 +0200

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product, Order
+from commands.models import Product, Order
 from django.contrib import messages
 
 @login_required
