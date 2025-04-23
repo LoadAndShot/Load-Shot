0bccfd744889cb48d700f284109ab1c1bf7055d6
LoadAndShot <loadandshot@gmail.com>
2025-04-23 12:51:01 +0200

    Update admin.py

--- commands/admin.py
+++ commands/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from .models import Product, Order
+from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'catalogue')
     list_filter = ('catalogue',)
     search_fields = ('name',)
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product', 'quantity', 'delivery_method', 'status', 'created_at')
-    list_filter = ('delivery_method', 'status')
-    search_fields = ('user__username', 'product__name')
 
