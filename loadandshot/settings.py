a6dfa4b0a97061d9f4f69f5d52dcd80baa07daa8
LoadAndShot <loadandshot@gmail.com>
2025-04-23 22:50:34 +0200

    Update settings.py

--- loadandshot/settings.py
+++ loadandshot/settings.py
@@ -64,6 +64,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # ← ajoute ça !
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
