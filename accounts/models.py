853c62a49ad3e9ff2c6a5cfdd1053b0ee4cf8340
LoadAndShot <loadandshot@gmail.com>
2025-04-25 16:02:11 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     can_access_catalogue1 = models.BooleanField(default=False)
     can_access_catalogue2 = models.BooleanField(default=False)
-    phone_number = models.CharField(max_length=50, blank=True, null=True)  # Numéro IG, libre
+    phone_number = models.CharField(max_length=128, blank=True, null=True)  # Numéro IG, libre
 
     def __str__(self):
         return self.username
