dbeeea676bdab07990aa86512dabb88862999c54
LoadAndShot <loadandshot@gmail.com>
2025-04-25 18:15:09 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=100, blank=True)
     can_access_catalogue1 = models.BooleanField(default=False)
     can_access_catalogue2 = models.BooleanField(default=False)
-    phone_number = models.CharField(max_length=128, blank=True, null=True)  # Numéro IG, libre
 
     def __str__(self):
         return self.username
